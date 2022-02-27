2248cc515175414f1100d44600ed89aa01e46f98
ayako202103 <s.hanada.a.202103@gmail.com>
2022-02-27 15:48:03 +0900

    制御構文2の課題

---
+++ 05/if2_1.php
@@ -0,0 +1,21 @@
+<?php
+
+$poket_money = 1000;
+$fund_raising = 101;
+
+while ($poket_money > $fund_raising) {
+    echo 'あなたの所持金は'. $poket_money. 'です'.'<br>';
+    echo $fund_raising .'募金しました'.'<br>';
+    $poket_money = $poket_money - $fund_raising;
+    echo '残りは'. $poket_money .'です。'. '<br>';
+}
+
+if ($poket_money < $fund_raising) {
+echo 'あなたはこれ以上募金できません。'. '<br>';
+}
+
+
+
+
+
+
